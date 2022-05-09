a00b1e04bfd7761f8b6b4f7dc4ce93f8b7c4ab2d
Kévin <kevin.jourdain@epitech.eu>
2022-05-09 15:58:04 +0200

    Add new project open new window

--- portfolio/src/components/MyProject.vue
+++ portfolio/src/components/MyProject.vue
@@ -6,21 +6,21 @@
       <div class="px-6 py-4">
         <div class="font-bold text-xl mb-2">Puissance 4</div>
         <p class="text-gray-700 text-base">
-          Ce système d’informations doit contenir les informations de jeu suivantes au minimum: <br> Pour chaque joueur: <br>• La couleur associée au joueur <br>
-          • L’id du joueur <br>Un indicateur d’état du tour avec: <br> • L’id du joueur courant <br> Un plateau de jeu contenant des cellules
-          Pour chaque cellule: <br> • Une coordonnée X <br> • Une coordonnée Y <br> Un état de la cellule pouvant être: <br> • VIDE
-          <br>• OU l’id du joueur
         </p>
       </div>
-      <div class="border-t py-8 px-6 md:px-12 md:py-16 lg:border-t-0 lg:border-l lg:py-0 lg:pr-0 lg:pl-6">
-        <a href="https://github.com/KevinJ-Dev/W-JSC-501-REN-1-1-puissance4-kj" class="block px-6 py-3 rounded bg-red-600 text-center text-white hover:bg-red-700">
-          Github
-        </a>
+      <div class="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
+        <div class="rounded-md shadow">
+          <a href="https://github.com/KevinJ-Dev/W-JSC-501-REN-1-1-puissance4-kj" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-red-600 hover:bg-red-700 md:py-4 md:text-lg md:px-10" target=newtab> Github </a>
+        </div>
+        <div class="mt-3 sm:mt-0 sm:ml-3">
+          <a href="https://github.com/KevinJ-Dev/W-JSC-501-REN-1-1-puissance4-kj/blob/master/README.md" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-red-600 hover:bg-red-700 md:py-4 md:text-lg md:px-10" target=newtab> Readme </a>
+        </div>
       </div>
       <div class="px-6 pt-4 pb-2">
         <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#HTML</span>
         <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#CSS</span>
         <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#JAVASCRIPT</span>
+        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#JQUERY</span>
       </div>
     </div>
 
@@ -30,18 +30,18 @@
       <div class="px-6 py-4">
         <div class="font-bold text-xl mb-2">Maquette Intro</div>
         <p class="text-gray-700 text-base">
-          En vous appuyant sur le visuel de la maquette, vous devez intégrer une page en HTML5 et CSS3 conforme
-          au visuel, respectant les normes du W3C et optimisant au mieux le référencement.
-          <br><br> Vous devez intégrer un player Youtube avec la vidéo de votre choix. Il se peut que l’affichage diffère légèrement de celui de l’exemple donné avec le sujet.
-          <br><br> Par ailleurs, il faut que la maquette soit “accessible”. Lors de la soutenance, nous poserons des questions
-          liées à “l’accessibilité”. <br><br> les polices à utiliser sont: Oswald, Droid sans et Droid Serif . A vous de trouver un moyen de les importer
-          pour votre intégration.
         </p>
       </div>
-      <div class="border-t py-8 px-6 md:px-12 md:py-16 lg:border-t-0 lg:border-l lg:py-0 lg:pr-0 lg:pl-6">
-        <a href="https://github.com/KevinJ-Dev/W-INT-501-REN-1-1-maquetteintro-kj" class="block px-6 py-3 rounded bg-red-600 text-center text-white hover:bg-red-700">
-          Github
-        </a>
+      <div class="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
+        <div class="rounded-md shadow">
+          <a href="https://github.com/KevinJ-Dev/W-INT-501-REN-1-1-maquetteintro-kj" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-red-600 hover:bg-red-700 md:py-4 md:text-lg md:px-10" target=newtab> Github </a>
+        </div>
+        <div class="mt-3 sm:mt-0 sm:ml-3">
+          <a href="https://kevinj-dev.github.io/W-INT-501-REN-1-1-maquetteintro-kj/" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-red-600 hover:bg-red-700 md:py-4 md:text-lg md:px-10" target=newtab> Demo </a>
+        </div>
+        <div class="mt-3 sm:mt-0 sm:ml-3">
+          <a href="https://github.com/KevinJ-Dev/W-INT-501-REN-1-1-maquetteintro-kj/blob/master/README.md" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-red-600 hover:bg-red-700 md:py-4 md:text-lg md:px-10" target=newtab> Readme </a>
+        </div>
       </div>
       <div class="px-6 pt-4 pb-2">
         <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#HTML</span>
@@ -56,23 +56,140 @@
       <div class="px-6 py-4">
         <div class="font-bold text-xl mb-2">Responsive</div>
         <p class="text-gray-700 text-base">
-          En vous appuyant sur le visuel de la maquette responsive, vous devez intégrer une page en HTML5 et CCS3
-          conforme au visuel, respectant les normes du W3C et optimisant au mieux le référencement.
-          <br><br> Le site doit être visible depuis un ordinateur, une tablette ou un téléphone. Les images doivent toujours
-          être affichées à 100%. C’est à dire que vous devez prévoir des images hautes et basses définitions. <br><br>Le redimensionnement du navigateur (de manière à émuler un autre périphérique) doit par conséquent influer sur
-          l’URL des images affichées.
-
         </p>
       </div>
-      <div class="border-t py-8 px-6 md:px-12 md:py-16 lg:border-t-0 lg:border-l lg:py-0 lg:pr-0 lg:pl-6">
-        <a href="https://github.com/KevinJ-Dev/W-INT-501-REN-1-1-responsive-kj" class="block px-6 py-3 rounded bg-red-600 text-center text-white hover:bg-red-700">
-          Github
-        </a>
+      <div class="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
+        <div class="rounded-md shadow">
+          <a href="https://github.com/KevinJ-Dev/W-INT-501-REN-1-1-responsive-kj" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-red-600 hover:bg-red-700 md:py-4 md:text-lg md:px-10" target=newtab> Github </a>
+        </div>
+        <div class="mt-3 sm:mt-0 sm:ml-3">
+          <a href="https://github.com/KevinJ-Dev/W-INT-501-REN-1-1-responsive-kj/blob/master/README.md" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-red-600 hover:bg-red-700 md:py-4 md:text-lg md:px-10" target=newtab> Readme </a>
+        </div>
       </div>
       <div class="px-6 pt-4 pb-2">
         <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#HTML</span>
         <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#CSS</span>
         <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#JAVASCRIPT</span>
+      </div>
+    </div>
+
+    <!--Card 4-->
+    <div class="rounded overflow-hidden shadow-lg">
+      <img class="w-full" src="../assets/Reservia.png" alt="Responsive">
+      <div class="px-6 py-4">
+        <div class="font-bold text-xl mb-2">Reservia</div>
+        <p class="text-gray-700 text-base">
+        </p>
+      </div>
+      <div class="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
+        <div class="rounded-md shadow">
+          <a href="https://github.com/KevinJ-Dev/Openclassrooms-P2" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-red-600 hover:bg-red-700 md:py-4 md:text-lg md:px-10" target=newtab> Github </a>
+        </div>
+        <div class="mt-3 sm:mt-0 sm:ml-3">
+          <a href="https://kevinj-dev.github.io/Openclassrooms-P2/" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-red-600 hover:bg-red-700 md:py-4 md:text-lg md:px-10" target=newtab> Demo </a>
+        </div>
+        <div class="mt-3 sm:mt-0 sm:ml-3">
+          <a href="https://github.com/KevinJ-Dev/Openclassrooms-P2/blob/master/README.md" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-red-600 hover:bg-red-700 md:py-4 md:text-lg md:px-10" target=newtab> Readme </a>
+        </div>
+      </div>
+      <div class="px-6 pt-4 pb-2">
+        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#HTML</span>
+        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#CSS</span>
+      </div>
+    </div>
+
+    <!--Card 5-->
+    <div class="rounded overflow-hidden shadow-lg">
+      <img class="w-full" src="../assets/Ohmyfood.png" alt="Responsive">
+      <div class="px-6 py-4">
+        <div class="font-bold text-xl mb-2">Ohmyfood</div>
+        <p class="text-gray-700 text-base">
+        </p>
+      </div>
+      <div class="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
+        <div class="rounded-md shadow">
+          <a href="https://github.com/KevinJ-Dev/Openclassrooms-P3" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-red-600 hover:bg-red-700 md:py-4 md:text-lg md:px-10" target=newtab> Github </a>
+        </div>
+        <div class="mt-3 sm:mt-0 sm:ml-3">
+          <a href="https://kevinj-dev.github.io/Openclassrooms-P3/" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-red-600 hover:bg-red-700 md:py-4 md:text-lg md:px-10" target=newtab> Demo </a>
+        </div>
+        <div class="mt-3 sm:mt-0 sm:ml-3">
+          <a href="https://github.com/KevinJ-Dev/Openclassrooms-P3/blob/master/README.md" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-red-600 hover:bg-red-700 md:py-4 md:text-lg md:px-10" target=newtab> Readme </a>
+        </div>
+      </div>
+      <div class="px-6 pt-4 pb-2">
+        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#HTML</span>
+        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#SCSS</span>
+      </div>
+    </div>
+
+    <!--Card 6--->
+    <div class="rounded overflow-hidden shadow-lg">
+      <img class="w-full" src="../assets/LaChouetteAgence.png" alt="Responsive">
+      <div class="px-6 py-4">
+        <div class="font-bold text-xl mb-2">La Chouette Agence</div>
+        <p class="text-gray-700 text-base">
+        </p>
+      </div>
+      <div class="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
+        <div class="rounded-md shadow">
+          <a href="https://github.com/KevinJ-Dev/La-chouette-agance-default" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-red-600 hover:bg-red-700 md:py-4 md:text-lg md:px-10" target=newtab> Github </a>
+        </div>
+        <div class="mt-3 sm:mt-0 sm:ml-3">
+          <a href="https://kevinj-dev.github.io/La-chouette-agance-default/" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-red-600 hover:bg-red-700 md:py-4 md:text-lg md:px-10" target=newtab> Demo </a>
+        </div>
+      </div>
+      <div class="px-6 pt-4 pb-2">
+        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#HTML</span>
+        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#CSS</span>
+        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#JAVASCRIPT</span>
+        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#SEO</span>
+      </div>
+    </div>
+    <!--Card 6--->
+    <div class="rounded overflow-hidden shadow-lg">
+      <img class="w-full" src="../assets/LaChouetteAgence.png" alt="Responsive">
+      <div class="px-6 py-4">
+        <div class="font-bold text-xl mb-2">Orinoco</div>
+        <p class="text-gray-700 text-base">
+        </p>
+      </div>
+      <div class="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
+        <div class="rounded-md shadow">
+          <a href="https://github.com/KevinJ-Dev/Openclassrooms-P5" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-red-600 hover:bg-red-700 md:py-4 md:text-lg md:px-10" target=newtab> Github </a>
+        </div>
+        <div class="mt-3 sm:mt-0 sm:ml-3">
+          <a href="https://github.com/KevinJ-Dev/Openclassrooms-P5/blob/master/README.md" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-red-600 hover:bg-red-700 md:py-4 md:text-lg md:px-10" target=newtab> Readme </a>
+        </div>
+      </div>
+      <div class="px-6 pt-4 pb-2">
+        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#HTML</span>
+        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#CSS</span>
+        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#JAVASCRIPT</span>
+        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#SEO</span>
+      </div>
+    </div>
+    <!--Card 7--->
+    <div class="rounded overflow-hidden shadow-lg">
+      <img class="w-full" src="../assets/LaChouetteAgence.png" alt="Responsive">
+      <div class="px-6 py-4">
+        <div class="font-bold text-xl mb-2">So Pekocko</div>
+        <p class="text-gray-700 text-base">
+        </p>
+      </div>
+      <div class="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
+        <div class="rounded-md shadow">
+          <a href="https://github.com/KevinJ-Dev/Openclassrooms-P6" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-red-600 hover:bg-red-700 md:py-4 md:text-lg md:px-10" target=newtab> Github </a>
+        </div>
+        <div class="mt-3 sm:mt-0 sm:ml-3">
+          <a href="https://github.com/KevinJ-Dev/Openclassrooms-P6/blob/master/README.md" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-red-600 hover:bg-red-700 md:py-4 md:text-lg md:px-10" target=newtab> Readme </a>
+        </div>
+      </div>
+      <div class="px-6 pt-4 pb-2">
+        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#HTML</span>
+        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#CSS</span>
+        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#JAVASCRIPT</span>
+        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#SEO</span>
       </div>
     </div>
   </div>
