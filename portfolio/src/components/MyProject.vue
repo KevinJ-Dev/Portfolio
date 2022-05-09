1902be28677b13b11724e822e9213f0d3fa9cdcc
Kévin <kevin.jourdain@epitech.eu>
2022-05-09 10:45:38 +0200

    Ad Project

--- portfolio/src/components/MyProject.vue
+++ portfolio/src/components/MyProject.vue
@@ -2,48 +2,70 @@
   <div class="p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-5">
     <!--Card 1-->
     <div class="rounded overflow-hidden shadow-lg">
-      <img class="w-full" src="../assets/test.png" alt="Reservia">
+      <img class="w-full" src="../assets/Puissance4.png" alt="Puissance">
       <div class="px-6 py-4">
-        <div class="font-bold text-xl mb-2">Reservia</div>
+        <div class="font-bold text-xl mb-2">Puissance 4</div>
         <p class="text-gray-700 text-base">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+          Ce système d’informations doit contenir les informations de jeu suivantes au minimum: <br> Pour chaque joueur: <br>• La couleur associée au joueur <br>
+          • L’id du joueur <br>Un indicateur d’état du tour avec: <br> • L’id du joueur courant <br> Un plateau de jeu contenant des cellules
+          Pour chaque cellule: <br> • Une coordonnée X <br> • Une coordonnée Y <br> Un état de la cellule pouvant être: <br> • VIDE
+          <br>• OU l’id du joueur
         </p>
       </div>
+      <div class="border-t py-8 px-6 md:px-12 md:py-16 lg:border-t-0 lg:border-l lg:py-0 lg:pr-0 lg:pl-6">
+        <a href="https://github.com/KevinJ-Dev/W-JSC-501-REN-1-1-puissance4-kj" class="block px-6 py-3 rounded bg-red-600 text-center text-white hover:bg-red-700">
+          Github
+        </a>
+      </div>
       <div class="px-6 pt-4 pb-2">
-        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#coucou</span>
-        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#coucou</span>
-        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#coucou</span>
+        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#HTML</span>
+        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#CSS</span>
+        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#JAVASCRIPT</span>
       </div>
     </div>
+
     <!--Card 2-->
     <div class="rounded overflow-hidden shadow-lg">
-      <img class="w-full" src="../assets/Moi.png" alt="Reservia">
+      <img class="w-full" src="../assets/MaquetteIntro.png" alt="Maquette">
       <div class="px-6 py-4">
-        <div class="font-bold text-xl mb-2">Reservia</div>
+        <div class="font-bold text-xl mb-2">Maquette Intro</div>
         <p class="text-gray-700 text-base">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+          En vous appuyant sur le visuel de la maquette responsive, vous devez intégrer une page en HTML5 et CCS3
+          conforme au visuel, respectant les normes du W3C et optimisant au mieux le référencement. <br><br> Le site doit être visible depuis un ordinateur, une tablette ou un téléphone. Les images doivent toujours
+          être affichées à 100%. C’est à dire que vous devez prévoir des images hautes et basses définitions. <br><br>Le redimensionnement du navigateur (de manière à émuler un autre périphérique) doit par conséquent influer sur
+          l’URL des images affichées.
         </p>
       </div>
+      <div class="border-t py-8 px-6 md:px-12 md:py-16 lg:border-t-0 lg:border-l lg:py-0 lg:pr-0 lg:pl-6">
+        <a href="https://github.com/KevinJ-Dev/W-INT-501-REN-1-1-maquetteintro-kj" class="block px-6 py-3 rounded bg-red-600 text-center text-white hover:bg-red-700">
+          Github
+        </a>
+      </div>
       <div class="px-6 pt-4 pb-2">
-        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#coucou</span>
-        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#coucou</span>
-        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#coucou</span>
+        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#HTML</span>
+        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#CSS</span>
+        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#BEM</span>
       </div>
     </div>
 
     <!--Card 3-->
     <div class="rounded overflow-hidden shadow-lg">
-      <img class="w-full" src="../assets/Moi.png" alt="Reservia">
+      <img class="w-full" src="../assets/MaquetteIntro.png" alt="Responsive">
       <div class="px-6 py-4">
-        <div class="font-bold text-xl mb-2">Reservia</div>
+        <div class="font-bold text-xl mb-2">Responsive</div>
         <p class="text-gray-700 text-base">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+          En
         </p>
       </div>
+      <div class="border-t py-8 px-6 md:px-12 md:py-16 lg:border-t-0 lg:border-l lg:py-0 lg:pr-0 lg:pl-6">
+        <a href="https://github.com/KevinJ-Dev/W-INT-501-REN-1-1-responsive-kj" class="block px-6 py-3 rounded bg-red-600 text-center text-white hover:bg-red-700">
+          Github
+        </a>
+      </div>
       <div class="px-6 pt-4 pb-2">
-        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#coucou</span>
-        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#coucou</span>
-        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#coucou</span>
+        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#HTML</span>
+        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#CSS</span>
+        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#JAVASCRIPT</span>
       </div>
     </div>
   </div>
