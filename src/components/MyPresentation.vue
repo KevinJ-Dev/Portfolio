cbd04bd86ad7da62045a2dad084fc70a4f64a322
Kévin <kevin.jourdain@epitech.eu>
2022-05-12 11:54:45 +0200

    Scrool to section

--- src/components/MyPresentation.vue
+++ src/components/MyPresentation.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="py-16 bg-white-50">
+  <div class="py-16 bg-white-50" id="presentation">
     <div class="container m-auto px-6 text-gray-600 md:px-12 xl:px-6">
       <div class="space-y-6 md:space-y-0 md:flex md:gap-6 lg:items-center lg:gap-12">
         <div class="md:5/12 lg:w-5/12">
